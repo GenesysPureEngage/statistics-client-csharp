db47f748be3a266c8e6722e7cc554543df4cc01e
maksimlysenko <maxim.lysenko@genesys.com>
2017-12-15 20:41:52 +0000

    9.0.000.00.750

--- src/Genesys.Internal.Statistics/Client/ApiClient.cs
+++ src/Genesys.Internal.Statistics/Client/ApiClient.cs
@@ -53,12 +53,12 @@ namespace Genesys.Internal.Statistics.Client
         public ApiClient()
         {
             Configuration = Genesys.Internal.Statistics.Client.Configuration.Default;
-            RestClient = new RestClient("http://gws-usw1.genhtcc.com/statistics/v3");
+            RestClient = new RestClient("http://your-server.com/statistics/v3");
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ApiClient" /> class
-        /// with default base path (http://gws-usw1.genhtcc.com/statistics/v3).
+        /// with default base path (http://your-server.com/statistics/v3).
         /// </summary>
         /// <param name="config">An instance of Configuration.</param>
         public ApiClient(Configuration config)
@@ -73,7 +73,7 @@ namespace Genesys.Internal.Statistics.Client
         /// with default configuration.
         /// </summary>
         /// <param name="basePath">The base path.</param>
-        public ApiClient(String basePath = "http://gws-usw1.genhtcc.com/statistics/v3")
+        public ApiClient(String basePath = "http://your-server.com/statistics/v3")
         {
            if (String.IsNullOrEmpty(basePath))
                 throw new ArgumentException("basePath cannot be empty");
